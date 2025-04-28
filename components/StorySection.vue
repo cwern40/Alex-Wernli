46df491cb893e8abe480fcf437537397fdcd5c47
cwern40 <chris.wernli@hotmail.com>
2025-04-28 16:02:16 -0600

    trying to fix images and small wording fix

--- components/StorySection.vue
+++ components/StorySection.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="flex gap-10 flex-col md:flex-row items-center justify-center w-full p-4 md:p-10 rounded-lg shadow-lg">
     <div class="basis-1/2 flex justify-center items-center" :class="{ 'order-0 md:order-1': imgPlacement === 'right' }">
-      <NuxtImg :src="content.imgSrc" :alt="content.imgAlt" sizes="320px" class="rounded-lg" loading="lazy" />
+      <NuxtImg
+        :src="content.imgSrc"
+        :alt="content.imgAlt"
+        sizes="320px"
+        format="webp"
+        class="rounded-lg"
+        loading="lazy"
+      />
     </div>
     <div class="basis-1/2 flex justify-center items-center">
       <p class="leading-loose">
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <section
     class="flex flex-col items-center justify-center w-full h-screen bg-center bg-no-repeat bg-cover sm:bg-contain"
-    style="
-      background-image: url('/img/Teenager/Alex_obituary.jpg');
-      background-color: #2a2a2a;
-      background-position: center;
-    "
+    :style="backgroundStyles"
   >
     <h2 class="leading-none text-white">THE MEMORIAL</h2>
     <h1 class="text-center text-[50px] sm:text-[100px] font-bold leading-none my-10 text-white">ALEX<br />WERNLI</h1>
@@ -17,14 +13,27 @@ definePageMeta({
   layout: 'no-header',
 });
 
+const img = useImage();
+
+const backgroundStyles = computed(() => {
+  const imgUrl = img('/img/Teenager/Alex_obituary.jpg', { format: 'webp' });
+
+  return { backgroundImage: `url('${imgUrl}')`, backgroundColor: '#2a2a2a', backgroundPosition: 'center' };
+});
+
 if (import.meta.server) {
   useSeoMeta({
     description:
-      'Alex Wernli was born June 1st 1992 and passed away March 12th 2012 at the age of 19 from brain cancer (DIPG). This is a memorial for him.',
+      'Alex Wernli was born June 1st 1992 and passed away March 12th 2012 at the age of 19 from brain cancer (DIPG). He never complained, choosing instead to use his radiant smile and infectious sense of humor to brighten the lives of those around him. This is his memorial.',
     ogDescription:
-      'Alex Wernli was born June 1st 1992 and passed away March 12th 2012 at the age of 19 from brain cancer (DIPG). This is a memorial for him.',
+      'Alex Wernli was born June 1st 1992 and passed away March 12th 2012 at the age of 19 from brain cancer (DIPG). He never complained, choosing instead to use his radiant smile and infectious sense of humor to brighten the lives of those around him. This is his memorial.',
     ogImage: '/img/Teenager/Alex_obituary.jpg',
-    url: 'https://alexwernli.com',
+    url: 'https://alexwernlimemorial.com',
   });
 }
+
+useSeoMeta({
+  title: "Alex Wernli's Memorial",
+  ogtitle: "Alex Wernli's Memorial",
+});
 </script>
