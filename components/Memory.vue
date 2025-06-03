150a343344cc5b3bb88790dfac61d7ba38c85d6a
cwern40 <chris.wernli@hotmail.com>
2025-06-02 20:17:47 -0600

    Adding memories for Whitney and Mary and setting a max length for the memories

--- components/Memory.vue
+++ components/Memory.vue
@@ -4,7 +4,18 @@
       <h3 class="text-center font-bold text-xl">{{ memory.author }}</h3>
       <h4 class="italic text-center">{{ memory.relationship }}</h4>
     </template>
-    <p v-for="p of memory.paragraphs">{{ p }}</p>
+    <p>
+      <span class="inline">{{ truncatedText }}</span>
+      <template v-if="overflowText">
+        <UButton v-if="!open" @click="open = true" variant="text" class="text-burnt-orange-500 inline py-0 pl-1 pr-0">
+          Read more
+        </UButton>
+        <span v-if="open" class="inline">{{ overflowText }}</span>
+        <UButton v-if="open" @click="open = false" variant="text" class="text-burnt-orange-500 inline py-0 pl-1 pr-0">
+          Less</UButton
+        >
+      </template>
+    </p>
   </UCard>
 </template>
 
@@ -14,6 +25,20 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+});
+
+const open = ref(false);
+
+const truncatedText = computed(() => {
+  const text = props.memory.paragraphs.join('\n');
+  const elipses = open.value ? '' : '...';
+  return text.length > 650 ? text.slice(0, 600) + elipses : text;
+});
+
+const overflowText = computed(() => {
+  const text = props.memory.paragraphs.join('\n');
+  if (text.length <= 650) return '';
+  return text.slice(600);
 });
 </script>
 
